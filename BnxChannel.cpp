422161f830fd80c97ca78a33d2c64799cf287822
nslay <nslay@users.noreply.github.com>
2013-01-20 03:48:44 +0000

    trunk: - Add change log for later releases. - Update copyrights on modified files.

--- BnxChannel.cpp
+++ BnxChannel.cpp
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2012 Nathan Lay (nslay@users.sourceforge.net)
+ * Copyright (c) 2012-2013 Nathan Lay (nslay@users.sourceforge.net)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
