48d3062b60c0dfa7e179ea92c245bcd48303908f
nslay <nslay@users.noreply.github.com>
2013-12-21 16:34:49 +0000

    ircbnx: - Add URL to version string.

--- BnxBot.cpp
+++ BnxBot.cpp
@@ -41,7 +41,8 @@
 std::string BnxBot::GetVersionString() {
 	std::stringstream versionStream;
 
-	versionStream << "IRCBNX Chatterbot " << MAJOR_VERSION << '.' << MINOR_VERSION << '.' << PATCH_LEVEL;
+	versionStream << "IRCBNX Chatterbot " << MAJOR_VERSION << '.' << MINOR_VERSION << '.' << PATCH_LEVEL  << '\n' <<
+		"http://sourceforge.net/projects/ircbnx/";
 
 	return versionStream.str();
 }
